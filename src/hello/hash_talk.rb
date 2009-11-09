3b9f8ae236b0366d6ba2cdc2a94fd5e938a03fcd
Jim Weirich <jim.weirich@gmail.com>
2009-11-09 04:36:12 +0000

    Moved some doodle code into src/hello

---
+++ src/hello/hash_talk.rb
@@ -0,0 +1,8 @@
+talks = {
+  "Jim" => "Ruby",
+  "Joe" => "Rails",
+  "Paul" => "Scottish Meatballs"
+}
+
+jims_talk = talks["Jim"]
+puts jims_talk
---
+++ src/hello/talk.rb
@@ -0,0 +1,12 @@
+class Talk
+  def initialize(name, speaker)
+    @name = name
+    @speaker = speaker
+  end
+  def name
+    @name
+  end
+  def speaker
+    @speaker
+  end
+end
