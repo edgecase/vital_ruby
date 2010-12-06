5384d810f37c821d0b41eea679f086f9b1f55fc7
Jim Weirich <jim.weirich@gmail.com>
2010-12-06 12:03:53 -0500

    Removed redundant presentation creation calls.

--- labs/lab03/solution/presentation_spec.rb
+++ labs/lab03/solution/presentation_spec.rb
@@ -10,32 +10,27 @@ describe Presentation do
   end
 
   it "can have scores added to it" do
-    p = Presentation.new("TITLE", "PRESENTER")
     lambda { presentation.add_score(4) }.should_not raise_error
   end
 
   it "can calculate the average score with a single score" do
-    p = Presentation.new("TITLE", "PRESENTER")
     presentation.add_score(4)
     presentation.average_score.should == 4
   end
 
   it "can calculate the average of two scores" do
-    p = Presentation.new("TITLE", "PRESENTER")
     presentation.add_score(4)
     presentation.add_score(5)
     presentation.average_score.should be_within(0.001).of(4.5)
   end
 
   it "rejects too large scores with an error" do
-    p = Presentation.new("TITLE", "PRESENTER")
     lambda {
       presentation.add_score(6)
     }.should raise_error(InvalidScoreError)
   end
 
   it "rejects too small scores with an error" do
-    p = Presentation.new("TITLE", "PRESENTER")
     lambda {
       presentation.add_score(0)
     }.should raise_error(InvalidScoreError)
